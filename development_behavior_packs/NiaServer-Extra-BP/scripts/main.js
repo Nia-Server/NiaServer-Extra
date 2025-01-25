004beb51b5dd4de77998188883cd9d756a11c07d
NIANIANKNIA <nianianknia@163.com>
2025-01-26 00:05:50 +0800

    feature: 添加海草丹和超级海草丹，移除任务相关代码，优化玩家效果处理逻辑

--- development_behavior_packs/NiaServer-Extra-BP/scripts/main.js
+++ development_behavior_packs/NiaServer-Extra-BP/scripts/main.js
@@ -2,22 +2,33 @@ import {world, system, ScreenDisplay} from "@minecraft/server";
 import { log,warn,error } from "./API/logger.js";
 import './newFunction.js';
 
-world.afterEvents.playerSpawn.subscribe((event) => {
-    if (event.initialSpawn) {
-        system.runTimeout(() => {
-            event.player.sendMessage("欢迎来到水世界！");
-        },100);
-    }
-});
-
 log("NiaServer-Extra加载完成！");
 
-//调试代码
-// system.runInterval(() => {
-//     for (const player of world.getAllPlayers()) {
-//         // player.sendMessage("这是一个定时任务");
-//         //player.runCommand('/fill ~8 ~8 ~8 ~-8 ~-8 ~-8 air replace mcnia:dark_raw_ore')
-//         player.runCommand('fill ~8 ~8 ~8 ~-8 ~-8 ~-8 air replace stone')
-//         player.runCommand('fill ~8 ~8 ~8 ~-8 ~-8 ~-8 air replace water')
-//     }
-// }, 10);
+const seaweed_potion_component = {
+    onConsume(event) {
+        let player = event.source;
+        if (player.isInWater) {
+            player.addEffect("minecraft:haste", 6000,{amplifier: 3, showParticles: false});
+            player.setDynamicProperty("mcnia:seaweed_potion", 300);
+            player.sendMessage("§7你感觉到一股力量在你的体内涌动...");
+        } else {
+            player.sendMessage("§7无事发生，尝试在水中试试？");}
+    }
+}
+
+world.beforeEvents.worldInitialize.subscribe(({ itemComponentRegistry }) => {
+    itemComponentRegistry.registerCustomComponent("mcnia:seaweed_potion", seaweed_potion_component);
+})
+
+system.runInterval(() => {
+    for (const player of world.getPlayers()) {
+        if (player.getDynamicProperty("mcnia:seaweed_potion") == undefined) continue;
+        if (player.getDynamicProperty("mcnia:seaweed_potion") <= 0) continue;
+        player.setDynamicProperty("mcnia:seaweed_potion", player.getDynamicProperty("mcnia:seaweed_potion") - 5);
+        if (!player.isInWater && player.getEffect("minecraft:haste") != undefined) {
+            player.removeEffect("minecraft:haste");
+            player.setDynamicProperty("mcnia:seaweed_potion", 0);
+            player.sendMessage("§7你感觉到一股力量在你的体内消散了...");
+        }
+    }
+}, 100);
