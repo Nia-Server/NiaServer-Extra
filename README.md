443c030c26b4dacde03dfc955d23e1c1a7d315a9
NIANIANKNIA <nianianknia@163.com>
2025-01-31 22:12:27 +0800

    feature: 添加海带丹和物品收集器的合成配方及相关纹理

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ NIA服务器额外扩展玩法相关实现代码
 
 本项目addons的完整运行**依赖于**主项目[NiaServer-Core](https://github.com/Nia-Server/NiaServer-Core)的addons包，如果你没有正确的安装主项目addons，可能出现部分功能异常。所以在安装本addons的同时请确保正确安装[NiaServer-Core](https://github.com/Nia-Server/NiaServer-Core)！
 
+**启用存档中所有BETA选项以及教育版模式**
+
 ## 特别感谢
 
 本项目中树生成逻辑使用了 [@happy1speed](https://mcpedl.com/user/happy1speed/) 制作的 [Atlas: Trees v4! Amplified Tree Generation! (MOVED)](https://mcpedl.com/atlas-trees-3-flourishing-trees/) 的大部分内容，并在此基础上进行了部分修改，本次使用已获得原作者的授权，如果需要未经修改的addons版本请前往原作者的mcpedl页面下载！
--- development_behavior_packs/NiaServer-Extra-BP/scripts/main.js
+++ development_behavior_packs/NiaServer-Extra-BP/scripts/main.js
@@ -1,4 +1,4 @@
-import {world, system, ScreenDisplay} from "@minecraft/server";
+import {world, system, ScreenDisplay, ItemComponentTypes} from "@minecraft/server";
 import { log,warn,error } from "./API/logger.js";
 import './newFunction.js';
 
@@ -9,25 +9,131 @@ const seaweed_potion_component = {
         let player = event.source;
         if (player.isInWater) {
             player.addEffect("minecraft:haste", 6000,{amplifier: 3, showParticles: false});
-            player.setDynamicProperty("mcnia:seaweed_potion", 300);
+            player.addEffect("minecraft:night_vision", 6000,{amplifier: 0, showParticles: false});
+            player.setDynamicProperty("sea_potion_time", 300);
             player.sendMessage("§7你感觉到一股力量在你的体内涌动...");
         } else {
-            player.sendMessage("§7无事发生，尝试在水中试试？");}
+            player.sendMessage("§7无事发生，尝试在水中试试？");
+        }
     }
 }
 
+const kelp_potion_small_component = {
+    onConsume(event) {
+        let player = event.source;
+        if (player.isInWater) {
+            player.addEffect("minecraft:haste", 6000,{amplifier: 3, showParticles: false});
+            player.addEffect("minecraft:night_vision", 6000,{amplifier: 0, showParticles: false});
+            player.setDynamicProperty("sea_potion_time", 300);
+            player.sendMessage("§7你感觉到一股力量在你的体内涌动...");
+        } else {
+            player.sendMessage("§7无事发生，尝试在水中试试？");
+        }
+    }
+}
+
+const kelp_potion_big_component = {
+    onConsume(event) {
+        let player = event.source;
+        if (player.isInWater) {
+            player.addEffect("minecraft:haste", 12000,{amplifier: 3, showParticles: false});
+            player.addEffect("minecraft:night_vision", 12000,{amplifier: 0, showParticles: false});
+            player.setDynamicProperty("sea_potion_time", 600);
+            player.sendMessage("§7你感觉到一股力量在你的体内涌动...");
+        } else {
+            player.sendMessage("§7无事发生，尝试在水中试试？");
+        }
+    }
+}
+
+const item_collector_small_component = {
+    onUse(event) {
+        event.source.sendMessage("§7物品收集器已开启...");
+        // event.itemStack.getComponent(ItemComponentTypes.Durability).damage = event.itemStack.getComponent(ItemComponentTypes.Durability).damage + 1;
+        // event.source.sendMessage(event.itemStack.getComponent(ItemComponentTypes.Durability).damage);
+        const item_collector_task_id = system.runInterval(() => {
+            let entity_list = event.source.getEntitiesFromViewDirection({"type": "item"});
+            if (entity_list.length == 0) {
+                return;
+            }
+            for (let i = 0; i < entity_list.length; i++) {
+                if (entity_list[i].distanse > 10) {
+                    continue;
+                }
+                entity_list[i].entity.teleport(event.source.location);
+            }
+        },1);
+        system.runTimeout(() => {
+            system.clearRun(item_collector_task_id);
+            event.source.sendMessage("§7物品收集器已关闭...");
+        }, 100);
+    }
+}
+
+const item_collector_middle_component = {
+    onUse(event) {
+        event.source.sendMessage("§7物品收集器已开启...");
+        const item_collector_task_id = system.runInterval(() => {
+            let entity_list = event.source.getEntitiesFromViewDirection({"type": "item"});
+            if (entity_list.length == 0) {
+                return;
+            }
+            for (let i = 0; i < entity_list.length; i++) {
+                if (entity_list[i].distanse > 20) {
+                    continue;
+                }
+                entity_list[i].entity.teleport(event.source.location);
+            }
+        },1);
+        system.runTimeout(() => {
+            system.clearRun(item_collector_task_id);
+            event.source.sendMessage("§7物品收集器已关闭...");
+        }, 200);
+    }
+}
+
+const item_collector_big_component = {
+    onUse(event) {
+        event.source.sendMessage("§7物品收集器已开启...");
+        const item_collector_task_id = system.runInterval(() => {
+            let entity_list = event.source.getEntitiesFromViewDirection({"type": "item"});
+            if (entity_list.length == 0) {
+                return;
+            }
+            for (let i = 0; i < entity_list.length; i++) {
+                if (entity_list[i].distanse > 20) {
+                    continue;
+                }
+                entity_list[i].entity.teleport(event.source.location);
+            }
+        },1);
+        system.runTimeout(() => {
+            system.clearRun(item_collector_task_id);
+            event.source.sendMessage("§7物品收集器已关闭...");
+        }, 300);
+    }
+}
+
+
+
 world.beforeEvents.worldInitialize.subscribe(({ itemComponentRegistry }) => {
     itemComponentRegistry.registerCustomComponent("mcnia:seaweed_potion", seaweed_potion_component);
+    itemComponentRegistry.registerCustomComponent("mcnia:kelp_potion_small", kelp_potion_small_component);
+    itemComponentRegistry.registerCustomComponent("mcnia:kelp_potion_big", kelp_potion_big_component);
+    itemComponentRegistry.registerCustomComponent("mcnia:item_collector_small", item_collector_small_component);
+    itemComponentRegistry.registerCustomComponent("mcnia:item_collector_middle", item_collector_middle_component);
+    itemComponentRegistry.registerCustomComponent("mcnia:item_collector_big", item_collector_big_component);
 })
 
 system.runInterval(() => {
     for (const player of world.getPlayers()) {
-        if (player.getDynamicProperty("mcnia:seaweed_potion") == undefined) continue;
-        if (player.getDynamicProperty("mcnia:seaweed_potion") <= 0) continue;
-        player.setDynamicProperty("mcnia:seaweed_potion", player.getDynamicProperty("mcnia:seaweed_potion") - 5);
-        if (!player.isInWater && player.getEffect("minecraft:haste") != undefined) {
+        if (player.getDynamicProperty("sea_potion_time") == undefined) continue;
+        if (player.getDynamicProperty("sea_potion_time") <= 0) continue;
+        player.setDynamicProperty("sea_potion_time", player.getDynamicProperty("sea_potion_time") - 5);
+        if (!player.isInWater && player.getEffect("minecraft:haste") != undefined && player.getEffect("minecraft:night_vision") != undefined) {
             player.removeEffect("minecraft:haste");
-            player.setDynamicProperty("mcnia:seaweed_potion", 0);
+            player.removeEffect("minecraft:night_vision");
+            player.setDynamicProperty("sea_potion_time", 0);
             player.sendMessage("§7你感觉到一股力量在你的体内消散了...");
         }
     }
