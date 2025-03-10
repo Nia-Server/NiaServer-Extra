aecd329b37b8e03ad85771888debcc7af602b967
NIANIANKNIA <nianianknia@163.com>
2025-03-10 21:56:32 +0800

    fix

--- development_behavior_packs/NiaServer-Extra-BP/scripts/output_GUI.js
+++ development_behavior_packs/NiaServer-Extra-BP/scripts/output_GUI.js
@@ -1,4 +1,4 @@
-import { system } from "@minecraft/server";
+import { world,system } from "@minecraft/server";
 import { EQGUI } from "./equipment_enhancement";
 import { error } from "./API/logger";
 
